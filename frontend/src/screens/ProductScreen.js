db1dd82d5b3b1d3cb2e59ccb21720412fd80427d
eligarlo <eligarlo.developer@gmail.com>
2020-10-12 05:47:35 +0300

    Fetching Products From React (useEffect)

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
 
 const ProductScreen = ({ match }) => {
-  const product = products.find(p => p._id === match.params.id)
+  const [product, setProduct] = useState({})
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`)
+
+      setProduct(data)
+    }
+
+    fetchProduct()
+  }, [])
+
   return (
     <>
       <Link className='btn btn-light my-3' to='/'>
@@ -21,7 +32,7 @@ const ProductScreen = ({ match }) => {
               <h3>{ product.name }</h3>
             </ListGroup.Item>
             <ListGroup.Item>
-              <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+              {product.rating && <Rating value={product.rating} text={`${product.numReviews} reviews`} />}
             </ListGroup.Item>
             <ListGroup.Item>
               Price: ${product.price}
